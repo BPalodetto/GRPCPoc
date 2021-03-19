33108348d5c4b3a8c62f8f3338764a5d7b3ce4c7
Bruno Palodetto <bruno.palodetto@animaeducacao.com.br>
2021-03-19 10:53:32 -0300

    Include project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+using GrpcHandsOn.Protos;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("http:localhost:5000");
+            var client = new Brooker.BrookerClient(channel);
+            var response = client.SendEmail(new Brookerequest());
+        }
+    }
+}
---
+++ GrpcHandsOn/Services/BrookerServiceGrpc.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using GrpcHandsOn.DomainService;
+using GrpcHandsOn.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcHandsOn.Services
+{
+    public class BrookerServiceGrpc : Brooker.BrookerBase
+    {
+        private readonly IEmailService _emailService;
+        public BrookerServiceGrpc(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public async override Task<Protos.HelloReply> SendEmail(Brookerequest request, ServerCallContext context)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+
+                throw new RpcException(new Status(StatusCode.Internal, ex.Message));
+            }
+
+        }
+    }
+}
